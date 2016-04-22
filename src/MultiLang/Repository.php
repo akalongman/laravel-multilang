53e0c8139abb2bfb949d6729b6f2020cf8a0fa04
LONGMAN <akalongman@gmail.com>
2016-04-22 19:46:30 +0400

    Added repository

---
+++ src/MultiLang/Repository.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * This file is part of the Laravel MultiLang package.
+ *
+ * (c) Avtandil Kikabidze aka LONGMAN <akalongman@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Longman\LaravelMultiLang;
+
+use Illuminate\Cache\CacheManager as Cache;
+use Illuminate\Database\DatabaseManager as Database;
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+use InvalidArgumentException;
+
+class Repository
+{
+
+
+
+
+}
---
+++ tests/Unit/RepositoryTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Cache\CacheManager as Cache;
+use Illuminate\Database\Schema\Blueprint;
+use Longman\LaravelMultiLang\MultiLang;
+
+class RepositoryTest extends AbstractTestCase
+{
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->createTable();
+    }
+
+    /**
+     * @test
+     */
+    public function some_test()
+    {
+
+
+    }
+
+
+
+
+}
