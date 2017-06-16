36193c626f32ea52c3602617b45664ed53e045bb
Avtandil Kikabidze <akalongman@gmail.com>
2017-06-16 15:24:09 +0400

    Remove redundant test

--- tests/Unit/ConfigTest.php
+++ tests/Unit/ConfigTest.php
@@ -8,21 +8,6 @@ class ConfigTest extends AbstractTestCase
     /**
      * @test
      */
-    public function create_with_invalid_data()
-    {
-        if (version_compare('7.0.0', PHP_VERSION, '>')) {
-            $this->markTestSkipped(
-                'Skipping test that can run only on a PHP 7.'
-            );
-        }
-        $this->setExpectedException('\ErrorException');
-
-        $config = $this->getConfig();
-    }
-
-    /**
-     * @test
-     */
     public function get_config_array()
     {
         $configData = [
